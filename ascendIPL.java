f005c5b7448a99c3b8b1cc94e305c60e1fe3d493
AryaPranesh <89635404+AryaPranesh@users.noreply.github.com>
2021-10-20 13:28:15 +0530

    Add files via upload

--- ascendIPL.java
+++ ascendIPL.java
@@ -1,5 +1,7 @@
 package IPL;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -10,7 +12,7 @@ import java.sql.Statement;
 public class ascendIPL {
 
 
-public static void main(String[] args) throws ClassNotFoundException, IOException {
+public static void main(String[] args) throws ClassNotFoundException, IOException, SQLException {
 	Connection myConn = null;
 	Statement myStmt = null;
 	String dbUrl = "jdbc:mysql://localhost:3306/ipl_teams";
@@ -30,14 +32,38 @@ public static void main(String[] args) throws ClassNotFoundException, IOExceptio
 	String teamname = rs.getString("Team_Name");
     String playername  = rs.getString("Player_Name");
     String playerscore = rs.getString("Player_Score");
+    System.out.println(teamid + "," + teamname + "," + playername + "," + playerscore);
+    String path = "C:\\\\Users\\\\arya.ramachandran\\\\Documents\\\\POC-DBData\\\\iplascend.txt";
 
+    FileWriter writer;
+    File file = new File(path);
+    writer = new FileWriter(file, true);  
+   while((rs!=null) && (rs.next()))
+   {
+   writer.write(rs.getString("Team_Id"));
+   writer.write("\t");
+   writer.write(rs.getString("Team_Name"));
+   writer.write("\t");
+   writer.write(rs.getString("Player_Name"));
+   writer.write("\t");
+   writer.write(rs.getString("Player_Score"));
+   }
 
-   System.out.println(teamid + "," + teamname + "," + playername + "," + playerscore);
-
+   writer.close();
 }
 }
 }catch(SQLException e) {
 e.printStackTrace();
+}finally {
+	
+if (myStmt != null) {
+myStmt.close();
 }
+
+}
+if (myConn != null) {
+myConn.close();
+}
+
 }
 }
